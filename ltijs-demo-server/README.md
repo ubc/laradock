9fc15e204b73b7607ee88c88ddf79b4ee762926b
John Hsu <john.hsu@ubc.ca>
2020-08-04 18:27:56 -0700

    ADD ltijs-demo-server, LTI tool to use for testing
    The LTI 1.3 PHP Example Tool has proven... problematic... in many areas.
    So I'm very happy to be able to replace it with the new Ltijs demo app.
    It supports all the services we want to use without us having to modify
    the source and also has a nice simple UI and logs that are far easier to
    debug. Also really helps that it tells you when requests encounter a
    server error, instead of just an unhelpful blank page.
    
    The ltijs-demo-server runs on port 4000, which none of the existing
    services seem to run on. It requires a mongo db server, which I've also
    added.
    
    I've modified index.js to add the shim as a platform entry. I've also
    added Dockerfile and corresponding docker-compose.yml for debugging use.
    This is based on the ltijs-demo-server repo with the latest commit on
    2020-07-29.
    
    OIDC Login: http://localhost:4000/login
    Auth Resp: http://localhost:4000/
    Target Link URI: http://localhost:4000/
    JWKS (within laradock): http://ltijs-demo-server:4000/keys
    ISS: http://localhost:4000/

---
+++ ltijs-demo-server/README.md
@@ -0,0 +1,34 @@
+<div align="center">
+	<br>
+	<br>
+	<a href="https://cvmcosta.github.io/ltijs"><img width="360" src="https://raw.githubusercontent.com/Cvmcosta/ltijs/987de79b9a3d529b1b507baa7b7a95d32ab386c2/docs/logo-300.svg?sanitize=true"></img></a>
+  <a href="https://site.imsglobal.org/certifications/coursekey/ltijs"​ target='_blank'><img width="80" src="https://www.imsglobal.org/sites/default/files/IMSconformancelogoREG.png" alt="IMS Global Certified" border="0"></img></a>
+</div>
+
+
+# Ltijs Demo Server
+
+> Ltijs v5 demo server
+
+### Usage
+
+- Download or clone the repo
+
+- Setup `.env` file with the relevant variables
+
+  ```
+  DB_HOST=localhost
+  DB_NAME=ltimoodle
+  DB_USER=user
+  DB_PASS=pass
+  LTI_KEY=LTIKEY
+  ```
+  *DB_USER and DB_PASS are not required*
+
+- Run `npm install`
+
+- Run `npm start` 
+
+### React application
+
+ The code for the react application used with this project can be found [here](https://github.com/Cvmcosta/ltijs-demo-client).
---
+++ ltijs-demo-server/index.js
@@ -0,0 +1,56 @@
+require('dotenv').config()
+const path = require('path')
+const routes = require('./src/routes')
+
+const lti = require('ltijs').Provider
+
+// Setup
+lti.setup(process.env.LTI_KEY,
+  {
+    url: 'mongodb://' + process.env.DB_HOST + '/' + process.env.DB_NAME + '?authSource=admin',
+    connection: { user: process.env.DB_USER, pass: process.env.DB_PASS }
+  }, {
+    staticPath: path.join(__dirname, './public'), // Path to static files
+    cookies: {
+      secure: false, // Set secure to true if the testing platform is in a different domain and https is being used
+      sameSite: '' // Set sameSite to 'None' if the testing platform is in a different domain and https is being used
+    },
+    devMode: false // Set DevMode to true if the testing platform is in a different domain and https is not being used
+  })
+
+// Whitelisting the main app route and /nolti to create a landing page
+lti.whitelist(lti.appRoute(), { route: new RegExp(/^\/nolti$/), method: 'get' }) // Example Regex usage
+
+// When receiving successful LTI launch redirects to app, otherwise redirects to landing page
+lti.onConnect(async (token, req, res, next) => {
+  if (token) return res.sendFile(path.join(__dirname, './public/index.html'))
+  else lti.redirect(res, '/nolti') // Redirects to landing page
+})
+
+// When receiving deep linking request redirects to deep link React screen
+lti.onDeepLinking(async (connection, request, response) => {
+  return lti.redirect(response, '/deeplink', { newResource: true })
+})
+
+// Setting up routes
+lti.app.use(routes)
+
+// Setup function
+const setup = async () => {
+  await lti.deploy({ port: process.env.PORT })
+
+  /**
+   * Register platform
+   */
+  await lti.registerPlatform({
+    url: 'http://localhost',
+    name: 'LTI Shim',
+    clientId: 'CLIENTID',
+    authenticationEndpoint: 'http://192.168.55.182/lti/launch/platform/auth',
+    accesstokenEndpoint: 'http://192.168.55.182/lti/security/platform/token',
+    authConfig: { method: 'JWK_SET',
+      key: 'http://192.168.55.182/lti/platform/jwks' }
+  })
+}
+
+setup()
