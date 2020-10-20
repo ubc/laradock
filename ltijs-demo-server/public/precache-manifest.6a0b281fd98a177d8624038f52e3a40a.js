4b53e58327e9735b5569b0394d3063033643a892
John Hsu <john.hsu@ubc.ca>
2020-10-20 00:43:51 -0700

    UPDATE ltijs demo server to 2010-09-14 commit
    ltijs itself jumped from v5.0 to v5.3
    
    The minified js now uses hardcoded http://localhost:3000/ API calls for
    some reason. Previously, it was just using relative paths, which didn't
    cause any issues. I had to edit the minified js to use relative paths
    again.

--- ltijs-demo-server/public/precache-manifest.6a0b281fd98a177d8624038f52e3a40a.js
+++ ltijs-demo-server/public/precache-manifest.6a0b281fd98a177d8624038f52e3a40a.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9ee3611f30e535366d9522c12d79c750",
+    "revision": "7861e5b8b2d6aa72aeac73880c6d744e",
     "url": "./index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/css/2.e2dd9b0a.chunk.css"
   },
   {
-    "revision": "237d47e353ea056d426a",
+    "revision": "f901d117d0cc260abd8b",
     "url": "./static/css/main.0e2988d0.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.9161100e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "237d47e353ea056d426a",
-    "url": "./static/js/main.bfc70ec6.chunk.js"
+    "revision": "f901d117d0cc260abd8b",
+    "url": "./static/js/main.1936bd6d.chunk.js"
   },
   {
     "revision": "b6d405b37e59947ce6c7",
--- ltijs-demo-server/public/service-worker.js
+++ ltijs-demo-server/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0ce5b3c3c236c431c2ed289eefdac5df.js"
+  "./precache-manifest.6a0b281fd98a177d8624038f52e3a40a.js"
 );
 
 self.addEventListener('message', (event) => {
